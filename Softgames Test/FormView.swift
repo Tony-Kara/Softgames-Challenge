7d9eba519c772b90753319e0ec8f790bf69d2efc
Eniola Anthony <eniolaanthony@gmail.com>
2022-05-30 03:47:21 +0600

    chore: refactor and clean up

--- Softgames Test/FormView.swift
+++ Softgames Test/FormView.swift
@@ -10,70 +10,59 @@ import WebKit
 
 class FormView: UIView {
   
-  // MARK: - Public properties
+  // MARK: - Private properties
   
-  func webToiOSSync(data: UserData) {
-    titleTextField.text = data.firstName
-//    emailTxtField.text = data.email
-//    lastnameTxtField.text = data.lastname
-    
-  }
-  
-  func webToiOSAysnc(data: UserData) {
-    titleTextField.text = data.firstName
-//    emailTxtField.text = data.email
-//    lastnameTxtField.text = data.lastname
-    
-  }
-  
-   var webView: WKWebView = {
-      let webConfiguration = WKWebViewConfiguration()
-      let webView = WKWebView(frame: .zero, configuration: webConfiguration)
-      webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-     webView.scrollView.isScrollEnabled = false
-     webView.scrollView.bounces = false
-     webView.translatesAutoresizingMaskIntoConstraints = false
-      return webView
+  var firstName : String?
+  var lastName : String?
+  var dateOfBirth : String?
+  var result : String?
+  let queue = DispatchQueue.global(qos: .userInitiated)
+   
+    private lazy var titleTextField:UITextField = {
+    let textField = UITextField()
+    textField.translatesAutoresizingMaskIntoConstraints = false
+    textField.placeholder = "Result field * Softgames *"
+    textField.keyboardType = UIKeyboardType.default
+    textField.returnKeyType = UIReturnKeyType.done
+    textField.autocorrectionType = UITextAutocorrectionType.no
+    textField.font = UIFont.systemFont(ofSize: 13)
+    textField.borderStyle = UITextField.BorderStyle.roundedRect
+    textField.clearButtonMode = UITextField.ViewMode.whileEditing;
+    textField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+    return textField
   }()
   
-  lazy var titleTextField:UITextField = {
-      let textField = UITextField()
-      textField.translatesAutoresizingMaskIntoConstraints = false
-     // textField.placeholder = "Title *"
-    //  textField.keyboardType = UIKeyboardType.default
-    //  textField.returnKeyType = UIReturnKeyType.done
-     // textField.autocorrectionType = UITextAutocorrectionType.no
-      textField.font = UIFont.systemFont(ofSize: 13)
-      textField.borderStyle = UITextField.BorderStyle.roundedRect
-    //  textField.clearButtonMode = UITextField.ViewMode.whileEditing;
-    //  textField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
-      return textField
-  }()
+  // MARK: - Public property
   
-  lazy var displayLabel : UILabel = {
-    let label = UILabel()
-    label.font = UIFont.systemFont(ofSize: 15, weight: .medium)
-    label.translatesAutoresizingMaskIntoConstraints = false
-   // label.text = "hello Yoo!!"
-    return label
+  var webView: WKWebView = {
+    let webConfiguration = WKWebViewConfiguration()
+    let webView = WKWebView(frame: .zero, configuration: webConfiguration)
+    webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    webView.scrollView.isScrollEnabled = false
+    webView.scrollView.bounces = false
+    webView.translatesAutoresizingMaskIntoConstraints = false
+    return webView
   }()
   
   // MARK: - Init
+  
   override init(frame: CGRect) {
     super.init(frame: frame)
     setupInitialLayout()
-     webView.isOpaque = false
+    backgroundColor = .white
+    webView.isOpaque = false
   }
-
+  
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-
+  
+  // MARK: - Private functions
+  
   private func setupInitialLayout() {
     addSubview(webView)
     addSubview(titleTextField)
-    
-    titleTextField.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 150).isActive = true
+    titleTextField.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 100).isActive = true
     titleTextField.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
     titleTextField.widthAnchor.constraint(equalToConstant: 300).isActive = true
     titleTextField.heightAnchor.constraint(equalToConstant: 100).isActive = true
@@ -82,8 +71,29 @@ class FormView: UIView {
     webView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
     webView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
     webView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor).isActive = true
-  
-    
   }
   
+  // MARK: - Public functions
+  
+  func webToiOSSync(data: UserData) {
+    self.firstName = data.firstName
+    self.lastName = data.lastname
+    guard let firstName = firstName, let lastName = lastName else {  return }
+    
+    queue.sync {
+      // Since i am performing a single task, i choosed not to use dispatch work item and since a user is tapping the button, i preferred using .userInitiated qos
+      self.result = firstName + " " + lastName
+      DispatchQueue.main.async {
+        self.titleTextField.text = self.result
+      }
+    }
+  }
+  
+  func webToiOSAysnc(data: UserData) {
+    self.dateOfBirth = data.dob
+    guard let dateOfBirth = dateOfBirth else { return }
+    DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+      self.titleTextField.text = dateOfBirth
+    }
+  }
 }
--- Softgames Test/ViewController.swift
+++ Softgames Test/ViewController.swift
@@ -8,21 +8,10 @@
 import UIKit
 import WebKit
 
-protocol StepByDelegate : class {
-    func updateUI(enable:Bool)
-    
-}
-
 class ViewController: UIViewController, UIWebViewDelegate {
   
-  
- 
-  weak var  delegate : StepByDelegate?
-  var jsonString = String()
   var mNativeToWebHandler : String = "sumbitToiOS"
   var NativeToWebHandlerAynsc : String = "sumbitToiOSAsync"
-
-
   let formView = FormView()
  
   override func loadView() {
@@ -30,11 +19,7 @@ class ViewController: UIViewController, UIWebViewDelegate {
   }
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
-    view.backgroundColor = .white
-    formView.webView.navigationDelegate = self
     loadWebKit()
-  
   }
 
   func loadWebKit() {
@@ -48,11 +33,9 @@ class ViewController: UIViewController, UIWebViewDelegate {
     let contentController = WKUserContentController()
     
     formView.webView.configuration.userContentController = contentController
-    
     formView.webView.configuration.userContentController.add(self, name: mNativeToWebHandler)
     formView.webView.configuration.userContentController.add(self, name: NativeToWebHandlerAynsc)
   }
-
 }
 
 //MARK: - Web view method to handle call backs
@@ -62,58 +45,22 @@ extension ViewController : WKScriptMessageHandler {
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
         print(message.body)
         let dict = message.body as? Dictionary<String, String>
-        print(dict)
-        
         let userdata = UserData((dict?["firstName"])!, (dict?["lastName"])!, (dict?["dob"])!)
-       
         if message.name == "sumbitToiOS" {
             self.sumbitToiOS(user: userdata)
         }
-      
       else if message.name == "sumbitToiOSAsync" {
         self.sumbitToiOSAsync(user: userdata)
       }
-        
     }
     
-    
     func sumbitToiOS(user:UserData){
-        //refresh token
-        print("sumbitToiOSSync")
         formView.webToiOSSync(data: user)
-        
     }
   
   func sumbitToiOSAsync(user:UserData){
-      //refresh token
-      print("sumbitToiOSAsync")
     formView.webToiOSAysnc(data: user)
-      
   }
-    
-    func endCurrentChat(isEnded: Bool){
-        self.navigationController?.popViewController(animated: true)
-    }
-    
-    
 }
 
-//MARK: - Web view delegate methods
 
-extension ViewController : WKNavigationDelegate {
-    
-    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
-    }
-    
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-   
-        print("didFinish")
-    }
-    
-    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
-        print(error.localizedDescription)
-    }
-    
-    
-    
-}
